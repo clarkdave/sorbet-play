4981c289286de4e744581da412e2bb3b16d8a127
clarkdave <me@clarkdave.net>
2019-09-16 18:38:45 +0100

    bug example

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sorbet-runtime', '~> 0.4'
 gem 'sorbet', '~> 0.4'
+
+gem 'bencode', '~> 0.8.2'
---
+++ main.rb
@@ -0,0 +1,12 @@
+# typed: true
+
+# 1. this should show documentation on hover (from the `sorbet-typed` defs) but
+#    it doesn't
+#
+# 2. "goto definition" goes to the auto-generated types in `rbi/gems` instead of
+#    those in `rbi/sorbet-types/lib/bencode`
+#
+# if you comment out line 12 in `rbi/gems/bencode.rbi` then the documentation
+# will show up as expected
+
+[].bencode
